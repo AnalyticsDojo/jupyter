16bdc637f86bb865d356f4a93fda45b763c2db67
Jason Kuruzovich <jkuruzovich@gmail.com>
2017-08-30 17:18:30 -0400

    Variety of updates to try to simplify.

--- travis/travis-script.bash
+++ travis/travis-script.bash
@@ -19,22 +19,4 @@ elif [[ ${ACTION} == 'deploy' ]]; then
     CHECKOUT_DIR="/tmp/${TRAVIS_BUILD_NUMBER}"
     COMMIT="${TRAVIS_COMMIT}"
     MASTER_HOST="datahub-fa17-${TRAVIS_BRANCH}.westus2.cloudapp.azure.com"
-    MASTER_HOST="datahub-fa17-${TRAVIS_BRANCH}.westus2.cloudapp.azure.com"
-    SSHKEY="sshkey-${TRAVIS_BRANCH}"
-
-    echo "Fetching ssh key..."
-    # Travis only allows encrypting one file per repo. LAME
-    openssl aes-256-cbc -K $encrypted_0f80927fa736_key -iv $encrypted_0f80927fa736_iv -in travis/ssh-keys.tar.gz.enc -out travis/ssh-keys.tar.gz -d
-    tar xvf travis/ssh-keys.tar.gz
-
-    chmod 0400 "${SSHKEY}"
-
-    echo "SSHing..."
-    ssh -i "${SSHKEY}"\
-        -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-        datahub@${MASTER_HOST} \
-        "git clone ${REPO} ${CHECKOUT_DIR} && cd ${CHECKOUT_DIR} && git checkout ${COMMIT} && git crypt unlock /etc/deploy-secret-keyfile && ./deploy.py deploy ${TRAVIS_BRANCH} && rm -rf ${CHECKOUT_DIR}"
-    rm -rf "sshkey-*"
-
-    echo "Done!"
 fi
